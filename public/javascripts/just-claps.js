90fa9484553cbb5d4e334abdcc862eb9fcc08349
Austin Mullins <austin.mullins@milsoft.com>
2017-05-19 15:10:57 -0500

    Add emoji options

--- public/javascripts/just-claps.js
+++ public/javascripts/just-claps.js
@@ -35,15 +35,16 @@ if (typeof module != "undefined" && module) {
 // Poor man's isomorphic Javascript
 if (typeof document != "undefined" && document) {
   var textArea = document.getElementById("text");
+  var symbolText = document.getElementById("symbol");
   var result = document.getElementById("result");
   var copyButton = document.getElementById("copy-button");
   var didCopySpan = document.getElementById("did-copy");
   var oldCopy = document.oncopy;
 
   if (textArea && result) {
-    textArea.addEventListener("input", function() {
-      result.innerHTML = justClapsTransform(textArea.value);
-    });
+    textArea.addEventListener("input", updateResult);
+    if (symbolText)
+      symbolText.addEventListener("input", updateResult);
   }
 
   if (copyButton) {
@@ -52,19 +53,62 @@ if (typeof document != "undefined" && document) {
       document.execCommand('copy');
     };
   }
-}
 
-function copyHandler(e) {
-  e.clipboardData.setData("text/plain", result.innerText);
-  e.preventDefault();
-  document.oncopy = oldCopy;
+  function copyHandler(e) {
+    e.clipboardData.setData("text/plain", result.innerText);
+    e.preventDefault();
+    document.oncopy = oldCopy;
 
-  didCopySpan.style.display = 'inline';
-  didCopySpan.style.opacity = 1.0;
-  setTimeout(function() {
-    didCopySpan.style.opacity = 0.0;
+    didCopySpan.style.display = 'inline';
+    didCopySpan.style.opacity = 1.0;
     setTimeout(function() {
-      didCopySpan.style.display = 'none';
+      didCopySpan.style.opacity = 0.0;
+      setTimeout(function() {
+        didCopySpan.style.display = 'none';
+      }, 500);
     }, 500);
-  }, 500);
+  }
+
+  function updateResult() {
+    var symbol = symbolText ? symbolText.value : HANDS;
+    result.innerHTML = justClapsTransform(textArea.value, symbol);
+  }
+
+  const SYMBOLS = ["👏", "👌", "🌈", "🌋", "🌠", "🌮", "🌹", "🍀", "💗"];
+  var symbolElements = [];
+  function makeSymbolElement(symbol) {
+    var el = document.createElement("a");
+    el.className = "symbol-btn";
+    el.innerText = symbol;
+    el.attributes["data-symbol"] = symbol;
+    el.attributes["href"] = "#";
+    el.onclick = symbolClick;
+    return el;
+  }
+
+  var collection = document.getElementById("symbol-collection");
+  SYMBOLS.forEach(function(symbol) {
+    var el = makeSymbolElement(symbol);
+    symbolElements.push(el);
+    collection.appendChild(el);
+  });
+
+  function symbolClick(e) {
+    updateSymbolClasses(this);
+    var symbol = this.attributes["data-symbol"];
+    if (symbol && symbolText) {
+      symbolText.value = symbol;
+    }
+    updateResult();
+  }
+
+  function updateSymbolClasses(selectedSymbol) {
+    symbolElements.forEach(function(el) {
+      if (el == selectedSymbol) {
+        el.className = "symbol-btn symbol-btn--selected";
+      } else {
+        el.className = "symbol-btn";
+      }
+    });
+  }
 }
